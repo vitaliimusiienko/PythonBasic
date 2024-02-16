a86cb80f00f54db73cc65eda53738eab0e5766f2
vitaliimusiienko <vitaliimusiienko10@gmail.com>
2024-02-16 18:38:40 +0200

    create first program and transfer to the dev branch

--- homework1.py
+++ homework1.py
@@ -1,2 +1,10 @@
 # Task1
 # Користувач вводить три цифри з клавіатури. Необхідно знайти сумму чисел та добуток. Результат вивести на екран
+
+n1 = int(input("Enter the first digit number: "))
+n2 = int(input("Enter the second digit number: "))
+n3 = int(input("Enter the third digit number: "))
+number_sum = n1 + n2 + n3
+number_mult = n1 * n2 * n3
+print(f"sum: {number_sum}")
+print(f"mult: {number_mult}")
