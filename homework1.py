05b335b5e850087a6f4ef2b0d4092c27ebe33bdc
vitaliimusiienko <vitaliimusiienko10@gmail.com>
2024-02-16 18:51:29 +0200

    writing a second task and solving it

--- homework1.py
+++ homework1.py
@@ -8,3 +8,10 @@ number_sum = n1 + n2 + n3
 number_mult = n1 * n2 * n3
 print(f"sum: {number_sum}")
 print(f"mult: {number_mult}")
+# Task2
+# Напишіть програму, яка обчислює площу ромба. Корситувач вводить довжину двох ййого діагоналей
+# Площа ромба дорівнює половині видобутку його діагоналей
+d1 = int(input("Enter the first diagonal: "))
+d2 = int(input("Enter the second diagonal: "))
+a1 = (d1 * d2) / 2
+print("Area:",a1)
