e33159cf821f234bed8b8508c0f6df24372a0076
vitaliimusiienko <vitaliimusiienko10@gmail.com>
2024-02-19 15:13:18 +0200

    adding a description of the first task and solving it

--- homework3.py
+++ homework3.py
@@ -4,6 +4,7 @@
 # Task1
 # Користувач вводить з клавіатури номер дня тижня (1-7). Необхідно вивести на екран назву дня тижня.
 # Наприклад якщо 1, на екрані напис понеділок, 2 - вівторок тощо.
+
 action = int(input("Enter a number from 1 to 7 to display the name of the day of the week: "))
 if action == 1:
     print("Monday")
@@ -12,7 +13,7 @@ if action == 2:
 if action == 3:
     print("Wednasday")
 if action == 4:
-    print ("Thurday")
+    print("Thurday")
 if action == 5:
     print("Friday")
 if action == 6:
