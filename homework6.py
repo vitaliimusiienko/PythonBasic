97510d2f81d1419acf3ba97808e9b9f0f4df9818
vitaliimusiienko <vitaliimusiienko10@gmail.com>
2024-03-04 16:46:56 +0200

    Added description of the third task and its completion

--- homework6.py
+++ homework6.py
@@ -24,3 +24,25 @@ def min_nums(numbers: list[int]) -> int:
             minimal_number = min(numbers)
 
     return minimal_number
+
+# Task3
+# Напишіть функцію, яка визначає кількість простих чисел у списку цілих. Список передається як параметр.
+# Отриманий результат повертається із функції.
+def prime_number(number: int) -> bool:
+    prime_number = True
+
+    for num in range(2, number):
+        if number % num == 0:
+            prime_number = False
+
+    return prime_number
+
+def prime_numbers_list(numbers:  list[int]) -> int:
+    prime_numbers_counter = 0
+
+    for num in numbers:
+        if prime_number(num):
+            prime_numbers_counter += 1
+
+    print()
+    return prime_numbers_counter
